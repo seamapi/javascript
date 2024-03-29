2e383d2d7e7ea4985dafe6167a2ee1d6f7b6e8f7
Mike Wu <hello@mikewu.dev>
2024-03-30 06:01:50 +0900

    feat: Generate README.md usage from dependencies (#52)
    * feat: generate README.md from dependencies
    
    * ci: Generate code
    
    * ci: Generate code
    
    * ci: Generate code
    
    * ci: Generate code
    
    * ci: Generate code
    
    * ci: Generate code
    
    * revert README.md
    
    * update to replace once
    
    * ci: Generate code
    
    * ci: Generate code
    
    * ci: Generate code
    
    * revert README.md
    
    * check generate loop
    
    * fix whitespace
    
    * ci: Generate code
    
    * move src/scripts -> scripts
    
    * move generate:readme -> above format
    
    * move generate-readme out of scripts
    
    * revert README.md
    
    * refactor to promises & specifying submodules
    
    * grab missing sections
    
    * replace types
    
    * skip type replacement
    
    * add back type replace
    
    * revert README.md
    
    * ci: Generate code
    
    * ci: Format code
    
    * ci: Generate code
    
    * ci: Format code
    
    * revert README.md
    
    * run generate
    
    * revert README.md
    
    * ignore whietspace
    
    * ci: Generate code
    
    * ci: Format code
    
    * remove current usage
    
    * Update generate-readme.ts
    
    Co-authored-by: Evan Sosenko <evan@getseam.com>
    
    * Update tsconfig.json
    
    Co-authored-by: Evan Sosenko <evan@getseam.com>
    
    * ignore genrate-readme in build
    
    * Update generate-readme.ts
    
    Co-authored-by: Evan Sosenko <evan@getseam.com>
    
    * use trim instead of regex
    
    * use trim instead of regex
    
    * revert readme
    
    * update ### Usage -> ## Usage
    
    * update ### Usage -> ## Usage
    
    * ci: Format code
    
    * ci: Generate code
    
    * ci: Format code
    
    * ci: Generate code
    
    * ci: Format code
    
    ---------
    
    Co-authored-by: Seam Bot <devops@getseam.com>
    Co-authored-by: Evan Sosenko <evan@getseam.com>

---
+++ generate-readme.ts
@@ -0,0 +1,71 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+const submodules = [
+  path.join('@seamapi', 'webhook'),
+  path.join('@seamapi', 'http'),
+]
+
+const sections = await Promise.all(submodules.map(readUsageSection))
+await writeReadmeUsage(sections.join('\n'))
+
+async function readUsageSection(modulePath: string): Promise<string> {
+  const data = await fs.readFile(
+    path.join('node_modules', modulePath, 'README.md'),
+    {
+      encoding: 'utf-8',
+    },
+  )
+
+  const regex = /#* Usage\s*([\s\S]*?(?=\n## \w))/
+  const matches = regex.exec(data)
+  if (matches == null || matches.length !== 2) {
+    throw new Error('Missing [## Usage] section')
+  }
+
+  const usage = matches[1]
+  if (usage == null) {
+    throw new Error('Invalid [## Usage] format')
+  }
+
+  return usage.trimStart().trimEnd()
+}
+
+async function writeReadmeUsage(content: string): Promise<void> {
+  const projectReadme = await fs.readFile('README.md', {
+    encoding: 'utf-8',
+  })
+
+  const usageRegex = /## Usage\s*([\s\S]*?(?=\n## Development))/
+
+  const matches = usageRegex.exec(projectReadme)
+
+  if (matches == null || matches.length !== 2 || matches[1] == null) {
+    throw new Error('Invalid README.md format')
+  }
+
+  const updatedContent = content
+    .replaceAll('@seamapi/webhook', 'seam')
+    .replaceAll('@seamapi/http', 'seam')
+    .replaceAll('SeamHttp', 'Seam')
+    .replaceAll('seam/connect', 'seam')
+
+  const currentUsageSection = matches[1]
+
+  if (
+    currentUsageSection
+      .replaceAll(/\s/g, '')
+      .includes(updatedContent.replaceAll(/\s/g, '')) // Remove all whitespace to ignore formatting changes
+  ) {
+    return
+  }
+
+  const injected = `## Usage
+
+${updatedContent}
+`
+
+  const result = projectReadme.replace(usageRegex, injected)
+
+  await fs.writeFile('README.md', result)
+}
